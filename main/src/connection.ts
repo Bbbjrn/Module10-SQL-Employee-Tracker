ea8d669ca798881fe54282543221e9876647ade3
Bbbjrn <bjorntimothyjohansson@gmail.com>
2024-08-20 10:37:18 -0600

    build out connection.ts

---
+++ main/src/connection.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+const connectToDb = async () => {
+    try {
+        await pool.connect();
+        console.log('Connected to database');
+    } catch (err) {
+        console.log('Error connecting to database', err);
+        Process.exit(1);
+ }
+};
+
+export { pool, connectToDb };
